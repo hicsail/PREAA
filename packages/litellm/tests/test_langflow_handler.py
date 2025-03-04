24d36d87d9b7fe65da737a6e38e7443b4028414e
Collin Bolles <collinbolles@gmail.com>
2025-03-04 15:51:10 -0500

    Fix formatting

--- packages/litellm/tests/test_langflow_handler.py
+++ packages/litellm/tests/test_langflow_handler.py
@@ -1,11 +1,10 @@
 import os
 os.environ['HELPER_BACKEND'] = 'test'
 
-from custom.langflow_handler import Langflow
+from custom.langflow_handler import Langflow  # noqa: E402
 
 
 def sample_test():
-
     langflow = Langflow()
     del langflow
     assert True
