1a862c32d9a559daec192deaf0103a50bd74f23d
Asad <am5815@bu.edu>
2025-03-05 11:37:36 -0500

    created schema and ingested it in module

--- packages/helper-backend/src/deepchat-proxy/deepchat-proxy.module.ts
+++ packages/helper-backend/src/deepchat-proxy/deepchat-proxy.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { DeepchatProxy, DeepchatProxySchema } from './deepchat-proxy.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: DeepchatProxy.name, schema: DeepchatProxySchema },
+    ]),
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
 export class DeepchatProxyModule {}
---
+++ packages/helper-backend/src/deepchat-proxy/deepchat-proxy.schema.ts
@@ -0,0 +1,19 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class DeepchatProxy {
+  /** The name the mapping should go by */
+  @Prop({ required: true })
+  model: string;
+
+  // url for the proxy
+  @Prop({ required: true })
+  url: string;
+
+  // API KEY to pass as header to LiteLLM
+  @Prop({ requied: true })
+  apiKey: string;
+}
+
+export type DeepchatProxyDocument = DeepchatProxy & Document;
+export const DeepchatProxySchema = SchemaFactory.createForClass(DeepchatProxy);
