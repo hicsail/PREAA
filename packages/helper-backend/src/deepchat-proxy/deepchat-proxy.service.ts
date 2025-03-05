296654c85c4877b9d7834decd3848d26ca4bf788
Asad <am5815@bu.edu>
2025-03-05 16:16:49 -0500

    indent fix

--- packages/helper-backend/src/deepchat-proxy/deepchat-proxy.service.ts
+++ packages/helper-backend/src/deepchat-proxy/deepchat-proxy.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from "@nestjs/common";
-import { DeepchatProxy, DeepchatProxyDocument } from "./deepchat-proxy.schema";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { LiteLLMService } from "src/litellm/litellm.service";
+import { Injectable } from '@nestjs/common';
+import { DeepchatProxy, DeepchatProxyDocument } from './deepchat-proxy.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { LiteLLMService } from 'src/litellm/litellm.service';
 
 @Injectable()
 export class DeepchatProxyService {
-  constructor(@InjectModel(DeepchatProxy.name) private readonly deepChatProxyModel: Model<DeepchatProxyDocument>,
-              private readonly liteLLMService: LiteLLMService) {}
+  constructor(
+    @InjectModel(DeepchatProxy.name)
+    private readonly deepChatProxyModel: Model<DeepchatProxyDocument>,
+    private readonly liteLLMService: LiteLLMService,
+  ) {}
 
   async get(id: string): Promise<DeepchatProxy | null> {
     return this.deepChatProxyModel.findOne({ _id: id });
@@ -22,20 +25,28 @@ export class DeepchatProxyService {
   }
 
   async update(mapping: DeepchatProxy): Promise<DeepchatProxy | null> {
-    return await this.deepChatProxyModel.findOneAndUpdate({ model: mapping.model }, mapping, {
-      new: true,
-      upsert: true,
-    });
+    return await this.deepChatProxyModel.findOneAndUpdate(
+      { model: mapping.model },
+      mapping,
+      {
+        new: true,
+        upsert: true,
+      },
+    );
   }
 
   async delete(model: string): Promise<void> {
     await this.deepChatProxyModel.deleteOne({ model });
   }
 
-  async proxyRequest(model: string, url: string, apiKey: string, body: any): Promise<any> {
+  async proxyRequest(
+    model: string,
+    url: string,
+    apiKey: string,
+    body: any,
+  ): Promise<any> {
     // reshape the body
     const response = this.liteLLMService.completion(model, apiKey, url, body);
     return response;
   }
-
 }
