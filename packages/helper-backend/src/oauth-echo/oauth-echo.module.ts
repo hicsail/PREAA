390fa5b23d28c37b34a73b4fd5869421e7f9dacc
Collin Bolles <collinbolles@gmail.com>
2025-03-12 11:33:45 -0400

    Remove old approach for oauth handling

--- packages/helper-backend/src/oauth-echo/oauth-echo.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OauthEchoController } from './oauth-echo.controller';
-import { OauthEchoService } from './oauth-echo.service';
-
-@Module({
-  controllers: [OauthEchoController],
-  providers: [OauthEchoService]
-})
-export class OauthEchoModule {}
