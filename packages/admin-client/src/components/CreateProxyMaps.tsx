e86c3f640b57911a5cb352c7ed69e3faddaebfe8
Asad <am5815@bu.edu>
2025-03-19 19:33:12 -0400

    added form to create proxy

--- packages/admin-client/src/components/CreateProxyMaps.tsx
+++ packages/admin-client/src/components/CreateProxyMaps.tsx
@@ -1,11 +1,11 @@
-import { Dialog, DialogTitle } from '@mui/material'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, FormHelperText, Input, InputLabel } from '@mui/material'
 
 interface Props {
   open: boolean;
-  setOpen: (open: boolean) => void;    
-} 
+  setOpen: (open: boolean) => void;
+}
 
-export default function CreateProxyMaps({open, setOpen}: Props) {
+export default function CreateProxyMaps({ open, setOpen }: Props) {
 
   const handleClose = () => {
     setOpen(false);
@@ -14,6 +14,33 @@ export default function CreateProxyMaps({open, setOpen}: Props) {
   return (
     <Dialog onClose={handleClose} open={open}>
       <DialogTitle>Create Proxy Mapping</DialogTitle>
+      <DialogContent>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="model-name">Model Name</InputLabel>
+          <Input id="model-name" />
+          <FormHelperText>
+            The name of the model from litellm you want to create a proxy for.
+          </FormHelperText>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="base-url">Base URL</InputLabel>
+          <Input id="base-url" />
+          <FormHelperText>
+            Base URL for the proxy mapping.
+          </FormHelperText>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="key">Lite LLM Key</InputLabel>
+          <Input id="key" />
+          <FormHelperText>
+            API Key for the model
+          </FormHelperText>
+        </FormControl>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button type="submit" variant="contained">Submit</Button>
+      </DialogActions>
     </Dialog>
   )
 }
