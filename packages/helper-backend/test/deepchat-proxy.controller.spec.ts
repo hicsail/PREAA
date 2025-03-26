cc5e1c30e1cc51d93a0399608cb338776e015cc7
Collin Bolles <collinbolles@gmail.com>
2025-03-26 12:09:26 -0400

    Get request

--- packages/helper-backend/test/deepchat-proxy.controller.spec.ts
+++ packages/helper-backend/test/deepchat-proxy.controller.spec.ts
@@ -3,6 +3,15 @@ import { DeepchatProxyController } from './deepchat-proxy.controller';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { DeepchatProxyService } from './deepchat-proxy.service';
 import { NotFoundException } from '@nestjs/common';
+import { DeepchatProxy } from './deepchat-proxy.schema';
+import mongoose from 'mongoose';
+
+const sampleProxy: DeepchatProxy = {
+  _id: new mongoose.Types.ObjectId('6401234567890abcdef12345'),
+  model: 'chatGPT',
+  url: 'http://example.com',
+  apiKey: 'test key'
+};
 
 describe('DeepchatProxyController', () => {
   let controller: DeepchatProxyController;
@@ -28,5 +37,11 @@ describe('DeepchatProxyController', () => {
     service.get.mockResolvedValue(null);
 
     await expect(controller.get('anything')).rejects.toThrow(NotFoundException);
+  });
+
+  it('should return mapping if found', async () => {
+    service.get.mockResolvedValue(sampleProxy);
+
+    await expect(controller.get('anything')).resolves.toBe(sampleProxy);
   });
 });
