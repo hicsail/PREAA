ef59d5e23a88be5b4257a004f4312f2e764646c3
Collin Bolles <collinbolles@gmail.com>
2025-03-26 15:02:30 -0400

    Add in model mock

--- packages/helper-backend/src/deepchat-proxy/deepchat-proxy.service.spec.ts
+++ packages/helper-backend/src/deepchat-proxy/deepchat-proxy.service.spec.ts
@@ -1,15 +1,24 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { DeepchatProxyService } from "./deepchat-proxy.service";
+import { getModelToken } from "@nestjs/mongoose";
+import { DeepchatProxy, DeepchatProxyDocument } from "./deepchat-proxy.schema";
+import { createMock, DeepMocked } from "@golevelup/ts-jest";
+import { Model } from "mongoose";
 
 describe('DeepchatSerivce', () => {
   let service: DeepchatProxyService;
+  let model: DeepMocked<Model<DeepchatProxyDocument>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DeepchatProxyService]
+      providers: [
+        DeepchatProxyService,
+        { provide: getModelToken(DeepchatProxy.name), useValue: createMock<Model<DeepchatProxyDocument>>() }
+      ]
     }).compile();
 
     service = module.get<DeepchatProxyService>(DeepchatProxyService);
+    model = module.get(getModelToken(DeepchatProxy.name));
   });
 
   it('should be defined', () => {
