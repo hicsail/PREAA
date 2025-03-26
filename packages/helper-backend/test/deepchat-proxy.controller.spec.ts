12f6e3e3905f01c1ee19c59f56c7d6c76163fe69
Collin Bolles <collinbolles@gmail.com>
2025-03-26 12:01:32 -0400

    Example rejection test

--- packages/helper-backend/test/deepchat-proxy.controller.spec.ts
+++ packages/helper-backend/test/deepchat-proxy.controller.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DeepchatProxyController } from './deepchat-proxy.controller';
-import { createMock } from '@golevelup/ts-jest';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { DeepchatProxyService } from './deepchat-proxy.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('DeepchatProxyController', () => {
   let controller: DeepchatProxyController;
+  let service: DeepMocked<DeepchatProxyService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,9 +17,16 @@ describe('DeepchatProxyController', () => {
     }).compile();
 
     controller = module.get<DeepchatProxyController>(DeepchatProxyController);
+    service = module.get(DeepchatProxyService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should throw not found if mapping does not exist', async () => {
+    service.get.mockResolvedValue(null);
+
+    await expect(controller.get('anything')).rejects.toThrow(NotFoundException);
   });
 });
