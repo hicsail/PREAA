6884eaf4d2d3963fb1c4490075a1155e90399a04
Collin Bolles <collinbolles@gmail.com>
2025-05-14 16:37:38 -0400

    Getting started with terraform

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "preaa-prod"
+    key     = "preaa/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      "Project" = "PREAA - Prod"
+    }
+  }
+}
