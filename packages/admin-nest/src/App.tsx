5d3501cf0ad71c1d4746dba21224849789c55e65
Collin Bolles <collinbolles@gmail.com>
2025-07-07 11:51:58 -0400

    Generate NextJS app

--- packages/admin-nest/src/App.tsx
+++ packages/admin-nest/src/App.tsx
@@ -2,9 +2,10 @@ import { Admin, Resource } from 'react-admin';
 import { Layout } from './Layout';
 import { dataProvider } from './dataProvider';
 import { ModelsList } from './components/models/ModelsList.component';
+import { ModelCreate } from './components/models/ModelCreate.component';
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="models" list={ModelsList} />
+    <Resource name="models" list={ModelsList} create={ModelCreate} />
   </Admin>
 );
--- packages/admin/eslint.config.mjs
+++ packages/admin/eslint.config.mjs
@@ -1,44 +1,16 @@
-import js from "@eslint/js";
-import { defineConfig, globalIgnores } from "eslint/config";
-import tseslint from "typescript-eslint";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import globals from "globals";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default defineConfig([
-  globalIgnores(["**/node_modules", "**/dist"]),
-  {
-    name: "eslint-js-recommended-rules",
-    plugins: {
-      js,
-    },
-    extends: ["js/recommended"],
-  },
-  tseslint.configs.recommended.map((conf) => ({
-    ...conf,
-    files: ["**/*.ts", "**/*.tsx"],
-  })),
-  eslintPluginPrettierRecommended,
-  {
-    name: "react",
-    ...react.configs.flat.recommended,
-  },
-  reactHooks.configs["recommended-latest"],
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off",
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  },
-]);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
