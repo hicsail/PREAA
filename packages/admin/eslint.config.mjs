1cdf824c195495902f2a2c2fc3a83677864a8ff8
Collin Bolles <collinbolles@gmail.com>
2025-07-10 16:09:39 -0400

    Allow unused with underscores

--- packages/admin/eslint.config.mjs
+++ packages/admin/eslint.config.mjs
@@ -15,7 +15,15 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
-      '@typescript-eslint/no-explicit-any': 'off'
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error', // or 'error'
+        {
+          'argsIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'caughtErrorsIgnorePattern': '^_'
+        }
+      ]
     },
   }),
 ];
