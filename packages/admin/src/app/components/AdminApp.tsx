ff4ba36a7a38b258ee4e83e1ec7bc2e53a22e141
Collin Bolles <collinbolles@gmail.com>
2025-07-14 14:52:34 -0400

    Working on oauth workflow

--- packages/admin/src/app/components/AdminApp.tsx
+++ packages/admin/src/app/components/AdminApp.tsx
@@ -1,19 +1,46 @@
 'use client';
+
 import { Admin, Resource } from 'react-admin';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { ModelsList } from './admin/models/ModelsList.component';
 import { ModelCreate } from './admin/models/ModelCreate.component';
 import simpleRestProvider from 'ra-data-simple-rest';
 import { ProxyCreate } from './admin/proxies/ProxyCreate.component';
 import { ProxiesList } from './admin/proxies/ProxiesList.component';
+import { getAuth } from './admin/auth';
+import { SessionProvider, useSession } from 'next-auth/react';
+import { AuthProvider } from 'react-admin';
 
 const dataProvider = simpleRestProvider('/api');
 
 const AdminApp: FC = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="models" list={ModelsList} create={ModelCreate} />
-    <Resource name="proxies" list={ProxiesList} create={ProxyCreate} />
-  </Admin>
+  <SessionProvider>
+    <InnerWrapper />
+  </SessionProvider>
 );
+
+const InnerWrapper: FC = () => {
+  const session = useSession();
+  const [authProvider, setAuthProvider] = useState<AuthProvider | null>(null);
+
+  useEffect(() => {
+    if (session.status === 'loading') {
+      return;
+    }
+    console.log(session);
+    setAuthProvider(getAuth(session));
+  }, [session]);
+
+  return (
+    <>
+      {authProvider &&
+        <Admin dataProvider={dataProvider} authProvider={authProvider}>
+          <Resource name="models" list={ModelsList} create={ModelCreate} />
+          <Resource name="proxies" list={ProxiesList} create={ProxyCreate} />
+        </Admin>
+      }
+    </>
+  );
+};
 
 export default AdminApp;
---
+++ packages/admin/src/app/components/admin/auth.ts
@@ -0,0 +1,28 @@
+import { AuthProvider } from 'react-admin';
+import { SessionContextValue, signIn, signOut } from 'next-auth/react'
+
+export const getAuth = (session: SessionContextValue): AuthProvider => {
+  return {
+    login: async () => {
+      await signIn('keycloak');
+    },
+    checkAuth: async () => {
+      console.log(session);
+      // If unauthenticated, redirect to login
+      if (session.status === 'unauthenticated') {
+        await signIn('keycloak');
+      }
+
+      // Otherwise do nothing yet
+    },
+    handleCallback: async () => {
+
+    },
+    logout: async () => {
+      await signOut();
+    },
+    checkError: async () => {
+
+    }
+  }
+};
