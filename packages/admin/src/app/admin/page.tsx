e3dcbe9bb9f3a5c6e7de7c3a933defa6e9b242bd
Asad <am5815@bu.edu>
2025-12-09 18:18:53 -0500

    keycloak auth added

--- packages/admin/src/app/admin/page.tsx
+++ packages/admin/src/app/admin/page.tsx
@@ -1,5 +1,14 @@
+import { getServerSession } from 'next-auth/next';
+import { redirect } from 'next/navigation';
+import { authOptions } from '../api/auth/[...nextauth]/route';
 import Admin from '../components/Admin';
 
-export default function Page() {
+export default async function AdminPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/');
+  }
+
   return <Admin />;
 }
